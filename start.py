51bf72d04c4b254abb864a640c50d7c1e550f5b3
Isack Nkolima <106377487+MrNkolima@users.noreply.github.com>
2023-01-29 18:55:44 +0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("cd 2019 && python3 necta.py")
+os.system("cd 2020 && python3 necta.py")
+os.system("cd 2021 && python3 necta.py")
+os.system("cd 2022 && python3 necta.py")
+print("DONEEEEEEE!!!!!")
